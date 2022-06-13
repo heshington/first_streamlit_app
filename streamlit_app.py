e636602484150d3e4a1d48cb3a6581d83db3fe82
Hiren Patel <hiren6@gmail.com>
2022-06-13 16:30:33 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 fruityvice_response = requests.put("https://fruityvice.com/api/fruit/" + add_my_fruit)
 streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute('insert into "PC_RIVERY_DB"."PUBLIC"."FRUIT_LOAD_LIST" values ('from streamlit');
+my_cur.execute('insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')");
 
